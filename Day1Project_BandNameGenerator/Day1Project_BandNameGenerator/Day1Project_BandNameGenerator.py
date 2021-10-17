03fdd42b46532bb977d0543e63583f7990a59cd7
Chris <52719722+blue1317@users.noreply.github.com>
2021-10-17 19:31:49 -0400

    day1

---
+++ Day1Project_BandNameGenerator/Day1Project_BandNameGenerator/Day1Project_BandNameGenerator.py
@@ -0,0 +1,6 @@
+
+print("Welcome to the Band Name Generator")
+cityName = input("What's the name of the city you grew up in: \n")
+petName = input("What is the name of your pet: \n")
+
+print("Your band name could be: " + cityName + " " + petName)
